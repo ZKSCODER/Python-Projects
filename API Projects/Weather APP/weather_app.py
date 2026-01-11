411fc29338da6af8751834b48e2d94813529ca53
Zaran Smith <zksmith@Zarans-MacBook-Pro.local>
2026-01-10 20:48:56 -0500

    Added error handling for weather app

--- API Projects/Weather APP/weather_app.py
+++ API Projects/Weather APP/weather_app.py
@@ -4,17 +4,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-city = input("Enter city name: ")
-api_key = os.getenv("MY_API_KEY") # Replace with your actual API key. You can remove "os.getenv" if code is not in a online 
-
 def get_weather(city: str):
-    base_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-    response = requests.get(base_url)
-    
-    return response.json()
+            base_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+            response = requests.get(base_url)
+            
+            return response.json()
+api_key = os.getenv("MY_API_KEY") # Replace with your actual API key. You can remove "os.getenv" if code is not in a online repository
 
-location_data = get_weather(city)
+while True:
+    try:
+        city1 = input("Enter city name: ")
+        city = city1.lower()
 
-print(f"City = {city}")
-print(f"Temperature = {location_data["main"]["temp"]}°F")
+        if city == "exit":
+            break
+        if not city:
+             print("Please enter a valid city name")
+             continue
+        
+        location_data = get_weather(city)
+        print(f"City = {city1}")
+        print(f"Temperature = {location_data["main"]["temp"]}°C")
+
+    except KeyError:
+        print("City not found. Please try again")
+        continue
     
+
+
